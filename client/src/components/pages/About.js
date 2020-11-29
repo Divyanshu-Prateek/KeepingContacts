9652aa749467d3d1aa50ec9618406fd2fec83def
Prateek Divyanshu <prateek10023@gmail.com>
2020-11-30 00:08:59 +0530

    Prepare to Deploy

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This Application</h1>
+      <p className="my-1">
+        This is Full Stack React Application for keeping contacts of people.
+      </p>
+      <p className="bg-dark p">
+        <strong>Version: 1.0.0</strong>
+      </p>
+    </div>
+  )
+}
+
+export default About;
--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path') // used in production time
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome To ContactKeeper API' }));
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({extended:false}));
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+
+// Serve  static assets in production
+if(process.env.NODE_ENV === 'production'){
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')));
+
+}
 
 const PORT = process.env.PORT || 5000;
 
